16c80a9f880fbeb3779dbf2452642ce8d6f3dd8c
dazer <nextorniquete@gmail.com>
2011-08-14 20:05:11 +0200

    Draft a header; will need to refactor it so it's reusable all throughout the views. Port strings to /res.

--- src/com/shadowcraft/android/ShadowcraftMain.java
+++ src/com/shadowcraft/android/ShadowcraftMain.java
@@ -2,12 +2,14 @@ package com.shadowcraft.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class shadowcraftActivity extends Activity {
+public class ShadowcraftMain extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
